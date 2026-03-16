96a64c32ced021d2034274ae3bd8d6e0ef7639f9
web1n <9633157+web1n@users.noreply.github.com>
2026-03-16 08:29:13 +0800

    Build natmapt-notify-script-bark

--- patch.sh
+++ patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --depth 1 --branch master https://github.com/muink/openwrt-natmapt.git package/natmapt
+git clone --depth 1 --branch bark https://github.com/web1n/openwrt-natmapt.git package/natmapt
 git clone --depth 1 --branch master https://github.com/muink/openwrt-stuntman.git package/stuntman
 git clone --depth 1 --branch master https://github.com/muink/luci-app-natmapt.git package/luci-app-natmapt
 
