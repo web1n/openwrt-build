c0171c01d8ad2c99394bc2417d5450cbda6ff460
web1n <9633157+web1n@users.noreply.github.com>
2026-03-17 11:11:33 +0800

    Build luci-app-cpu-status

--- patch.sh
+++ patch.sh
@@ -4,6 +4,9 @@
 git clone --depth 1 --branch main https://github.com/timsaya/openwrt-bandix.git package/bandix
 git clone --depth 1 --branch main https://github.com/timsaya/luci-app-bandix.git package/luci-app-bandix
 
+# cpu-status
+git clone --depth 1 --branch master https://github.com/gSpotx2f/luci-app-cpu-status.git package/luci-app-cpu-status
+
 # natmapt
 git clone --depth 1 --branch bark https://github.com/web1n/openwrt-natmapt.git package/natmapt
 git clone --depth 1 --branch master https://github.com/muink/openwrt-stuntman.git package/stuntman
