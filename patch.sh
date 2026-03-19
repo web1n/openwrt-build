b8a381dfbbc3e7142e80da14e9d4eeea5a535107
web1n <9633157+web1n@users.noreply.github.com>
2026-03-19 17:45:12 +0800

    Build natmapt-notify-script-edgeone

--- patch.sh
+++ patch.sh
@@ -14,6 +14,6 @@ git clone --depth 1 --branch main https://github.com/yingziwu/openwrt-fakehttp.g
 git clone --depth 1 --branch main https://github.com/yingziwu/luci-app-fakehttp.git package/luci-app-fakehttp
 
 # natmapt
-git clone --depth 1 --branch bark https://github.com/web1n/openwrt-natmapt.git package/natmapt
+git clone --depth 1 --branch master https://github.com/web1n/openwrt-natmapt.git package/natmapt
 git clone --depth 1 --branch master https://github.com/muink/openwrt-stuntman.git package/stuntman
 git clone --depth 1 --branch master https://github.com/muink/luci-app-natmapt.git package/luci-app-natmapt
