2d0c62b31752b665810299ac418de7039abe11fe
web1n <9633157+web1n@users.noreply.github.com>
2026-05-12 10:40:07 +0800

    Revert "Build bandix for arm64"
    This reverts commit b539727c41ea25231fd66bfcb10521841c861880.

--- patch.sh
+++ patch.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# bandix
-git clone --depth 1 --branch main https://github.com/timsaya/openwrt-bandix.git package/bandix
-git clone --depth 1 --branch main https://github.com/timsaya/luci-app-bandix.git package/luci-app-bandix
-
 # cpu-status
 git clone --depth 1 --branch master https://github.com/gSpotx2f/luci-app-cpu-status.git package/luci-app-cpu-status
 
