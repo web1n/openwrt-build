6ddad30024293cc0aea4d40397569594565f833f
web1n <9633157+web1n@users.noreply.github.com>
2026-05-12 10:39:31 +0800

    Revert "Build fakehttp"
    This reverts commit a541ebe1aaa7cddef1e5bc4e05a039b46ef3483f.

--- patch.sh
+++ patch.sh
@@ -9,10 +9,6 @@ git clone --depth 1 --branch main https://github.com/timsaya/luci-app-bandix.git
 # cpu-status
 git clone --depth 1 --branch master https://github.com/gSpotx2f/luci-app-cpu-status.git package/luci-app-cpu-status
 
-# fakehttp
-git clone --depth 1 --branch main https://github.com/yingziwu/openwrt-fakehttp.git package/fakehttp
-git clone --depth 1 --branch main https://github.com/yingziwu/luci-app-fakehttp.git package/luci-app-fakehttp
-
 # natmapt
 git clone --depth 1 --branch master https://github.com/web1n/openwrt-natmapt.git package/natmapt
 git clone --depth 1 --branch master https://github.com/muink/openwrt-stuntman.git package/stuntman
