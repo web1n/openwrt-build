504e1f82ca34940507549607c13be1e6904587ea
web1n <9633157+web1n@users.noreply.github.com>
2026-06-24 16:16:39 +0800

    Build luci-app-uugamebooster

--- patch.sh
+++ patch.sh
@@ -9,3 +9,10 @@ git clone --depth 1 --branch master https://github.com/gSpotx2f/luci-app-cpu-sta
 git clone --depth 1 --branch master https://github.com/web1n/openwrt-natmapt.git package/natmapt
 git clone --depth 1 --branch master https://github.com/muink/openwrt-stuntman.git package/stuntman
 git clone --depth 1 --branch master https://github.com/muink/luci-app-natmapt.git package/luci-app-natmapt
+
+# uu game booster
+git clone --depth 1 --branch openwrt-25.12 https://github.com/coolsnowwolf/luci /tmp/coolsnowwolf-luci
+git clone --depth 1 --branch master https://github.com/coolsnowwolf/packages /tmp/coolsnowwolf-packages
+cp -r /tmp/coolsnowwolf-luci/applications/luci-app-uugamebooster package/luci-app-uugamebooster
+cp -r /tmp/coolsnowwolf-packages/net/uugamebooster package/uugamebooster
+rm -rf /tmp/coolsnowwolf-luci /tmp/coolsnowwolf-packages
