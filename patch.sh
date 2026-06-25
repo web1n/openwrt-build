bed35ac6f15f62df28e1fac0880e6c5df4896475
web1n <9633157+web1n@users.noreply.github.com>
2026-06-25 09:53:01 +0800

    Fix LuCI package makefile include

--- patch.sh
+++ patch.sh
@@ -16,3 +16,12 @@ git clone --depth 1 --branch master https://github.com/coolsnowwolf/packages /tm
 cp -r /tmp/coolsnowwolf-luci/applications/luci-app-uugamebooster package/luci-app-uugamebooster
 cp -r /tmp/coolsnowwolf-packages/net/uugamebooster package/uugamebooster
 rm -rf /tmp/coolsnowwolf-luci /tmp/coolsnowwolf-packages
+
+# fix LuCI packages copied from feeds/luci/applications
+for makefile in package/luci-app-*/Makefile; do
+    [ -f "$makefile" ] || continue
+    if grep -q 'include ../../luci.mk' "$makefile"; then
+        echo "Fix luci.mk include: $makefile"
+        sed -i 's|include ../../luci.mk|include $(TOPDIR)/feeds/luci/luci.mk|' "$makefile"
+    fi
+done
